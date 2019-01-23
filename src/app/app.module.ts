deb04caedc8bdef92b9c3929421c7b56c113c5b8
Akash Sengar <akashsengar96@gmail.com>
2019-01-24 01:49:54 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { SuperTabsModule } from 'ionic2-super-tabs';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -21,6 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    SuperTabsModule.forRoot(),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
-
+import {SuperTabsController} from 'ionic2-super-tabs';
 @Component({
   templateUrl: 'tabs.html'
 })
@@ -13,7 +13,19 @@ export class TabsPage {
   tab2Root = AboutPage;
   tab3Root = ContactPage;
 
-  constructor() {
+  constructor(public superTabsCtrl: SuperTabsController) {
 
+  }
+
+  hideToolbar() {
+    this.superTabsCtrl.showToolbar(false);
+  }
+
+  showToolbar() {
+    this.superTabsCtrl.showToolbar(true);
+  }
+
+  onTabSelect(ev: any) {
+    console.log('Tab selected', 'Index: ' + ev.index, 'Unique ID: ' + ev.id);
   }
 }
